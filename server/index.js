576ec7c93520d8e2c8ab45216e69e1b8f537f930
Simon <simonyuanliu@gmail.com>
2018-11-06 19:49:24 -0800

    finally got the server to work

--- server/index.js
+++ server/index.js
@@ -2,33 +2,38 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const request = require('request');
+const http = require('http');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-app.get('/:id', (req, res) => {
-  const number = req.params.id;
-  res.redirect(`http://localhost:3001/grub-reactor/${number}/`);
+app.use(morgan('dev'));
+app.use('/:id', express.static(path.join(__dirname, '../', 'public')));
+app.get(`/grub-reactor/:menuId/menu`, (req, res) => {
+  console.log('hi');
+  var menuInfo = {
+    host: 'localhost',
+    port: 3001,
+    path: `/grub-reactor/${req.params.menuId}/menu`,
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    }
+  };
+  var menuInfo = http.request(menuInfo, (result) => {
+    result.on('data', (data) => {
+      res.status(200).send(data);
+    }).on('error', (err) => {
+      res.status(500).send("menu not found");
+    });
+  });
+  menuInfo.end();
 });
 
-// app.get('', (req, res) => {
-//   const {number} = req.params;
-//   res.redirect(``);
-// });
-// app.get('', (req, res) => {
-//   const {number} = req.params;
-//   res.redirect(``);
-// });
 
-
-// app.get('', (req, res) => {
-//   const {number} = req.params;
-//   res.redirect(``);
-// });
-// app.get('', (req, res) => {
-//   const {number} = req.params;
-//   res.redirect(``);
-//});
 
 
 
